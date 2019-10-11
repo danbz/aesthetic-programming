10932607480fae6ff34c6fb8464229daae873e7d
soon <rwx@siusoon.net>
2019-10-11 17:43:14 +0000

    Upload New File

---
+++ p5_SampleCode/ch3_InfiniteLoops/sketch3_1/sketch.js
@@ -0,0 +1,36 @@
+//ch3_1
+function setup() {
+ createCanvas(windowWidth, windowHeight);   //create a drawing canvas
+ frameRate (8);  //try to change this parameter
+}
+
+function draw() {
+  fill(70,80);  //check this syntax with alpha value
+  noStroke();
+  rect(0, 0, width, height);
+  drawElements();
+}
+
+function drawElements() {
+  let num =9;
+  push();
+  translate(width/2, height/2); //move things to the center
+  // 360/num >> degree of each ellipse' move ;frameCount%num >> get the remainder that indicates the movement of the ellipse
+  let cir = 360/num*(frameCount%num);  //to know which one among 8 possible positions.
+  rotate(radians(cir));
+  noStroke();
+  fill(255,255,0);
+  ellipse(35,0,22,22);  //the moving dot(s), the x is the distance from the center
+  pop();
+
+  stroke(255,255,0, 18);
+  //static lines
+  line(60,0,60,height);
+  line(width-60,0,width-60,height);
+  line(0,60,width,60);
+  line(0,height-60,width,height-60);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
