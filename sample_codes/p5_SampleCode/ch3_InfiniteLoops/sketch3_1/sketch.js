86c1b72f6ec4ade37920e566d3b64fabab86baca
Stephanie Vilayphiou <stephanie@stdin.fr>
2019-10-18 15:19:24 +0200

    Made the throbber sketch black and white

--- sample_codes/p5_SampleCode/ch3_InfiniteLoops/sketch3_1/sketch.js
+++ sample_codes/p5_SampleCode/ch3_InfiniteLoops/sketch3_1/sketch.js
@@ -5,30 +5,25 @@ function setup() {
 }
 
 function draw() {
-  fill(70,80);  //check this syntax with alpha value
+  fill(255, 255, 255, 100);  //check this syntax with alpha value
   noStroke();
   rect(0, 0, width, height);
   drawElements();
 }
 
 function drawElements() {
-  let num =9;
+  let num = 9;
   push();
   translate(width/2, height/2); //move things to the center
   // 360/num >> degree of each ellipse' move ;frameCount%num >> get the remainder that indicates the movement of the ellipse
   let cir = 360/num*(frameCount%num);  //to know which one among 8 possible positions.
   rotate(radians(cir));
   noStroke();
-  fill(255,255,0);
+  fill(0,0,0);
   ellipse(35,0,22,22);  //the moving dot(s), the x is the distance from the center
   pop();
 
-  stroke(255,255,0, 18);
-  //static lines
-  line(60,0,60,height);
-  line(width-60,0,width-60,height);
-  line(0,60,width,60);
-  line(0,height-60,width,height-60);
+  stroke(0,0,0, 18);
 }
 
 function windowResized() {
