bc4dfcc06286b7e213719a444cc27335c63d7518
soon <rwx@siusoon.net>
2020-01-09 07:05:46 +0000

    Update sketch.js

--- sample_codes/p5_SampleCode/ch3_InfiniteLoops/sketch.js
+++ sample_codes/p5_SampleCode/ch3_InfiniteLoops/sketch.js
@@ -49,6 +49,7 @@ function setup(){
 function draw(){
      currentMillis = floor(millis() - milliStart);  //millis means millsecond since starting the program, like frameCount
      if(currentMillis > timer){
+       push()
        translate(xPos[xCtr], yPos[yCtr]);  //rows and cols
        rotate(radians((360/8)* (millis()/speed)));  //rotate in itself
        timer = currentMillis + speed; //the time for the next loop
